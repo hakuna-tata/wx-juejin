7a9dce3bceea0c654bafe568e57efc15f2e48410
fffxue <18897960357@163.com>
2019-10-12 18:10:17 +0800

    card component

---
+++ components/card/index.js
@@ -0,0 +1,15 @@
+// components/card/index.js
+Component({
+    properties: {
+        count:Number
+    },
+
+    data: {
+
+    },
+    methods: {
+        tapCardItem(e){
+            console.log(e)
+        }
+    }
+})
